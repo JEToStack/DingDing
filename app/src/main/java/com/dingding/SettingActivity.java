e95993f9f77c660706c32ab7894fd1a2889b0102
Savong Sam <samsavongsr@gmail.com>
2018-06-24 09:12:21 +0700

    edited SettingActivity.java

--- app/src/main/java/com/dingding/SettingActivity.java
+++ app/src/main/java/com/dingding/SettingActivity.java
@@ -1,14 +1,35 @@
 package com.dingding;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ImageView;
 
-public class SettingActivity extends Activity {
+public class SettingActivity extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+//        reference to back imageView
+        ImageView imgBack = findViewById(R.id.img_back_in_setting);
+//        set listener to back ImageView
+        imgBack.setOnClickListener(this);
+    }
+
+    private void startBackActivity(){
+        finish();
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view.getId() == R.id.img_back_in_setting){
+            startBackActivity();
+        }
+
     }
 }
